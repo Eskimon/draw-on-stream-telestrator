3c7aa0f236d739877bb0da6becdcf1a4715c3846
Eskimon <eskimon@outlook.com>
2019-09-19 11:28:33 +0200

    two files and socket link

---
+++ commander.py
@@ -0,0 +1,187 @@
+import socket
+import queue
+import threading
+import time
+import tkinter as tk
+from tkinter.colorchooser import askcolor
+import tkinter.font as tkFont
+
+
+the_queue = queue.Queue()
+
+HOST = 'localhost'
+PORT = 4816
+
+
+class Commander():
+
+    DEFAULT_PEN_SIZE = 5.0
+    DEFAULT_COLOR = 'black'
+    BG_COLOR = 'white'
+
+    mode = 'pen'
+
+    def __init__(self):
+        self.root = tk.Tk()
+
+        # Load config.json
+        # Place the window
+        # self.root.geometry("1000x900+10+10")
+
+        # Some variables
+        self.items = []
+        self.font = tkFont.Font(family='Helvetica', size=20)
+        self.text_input = tk.StringVar(self.root)
+
+        # Build the interface
+        self.pen_button = tk.Button(self.root, text='pen', command=self.use_pen)
+        self.pen_button.grid(row=0, column=0)
+
+        self.rectangle_button = tk.Button(self.root, text='rect', command=self.use_rect)
+        self.rectangle_button.grid(row=0, column=1)
+
+        self.ellipse_button = tk.Button(self.root, text='ellipse', command=self.use_ellipse)
+        self.ellipse_button.grid(row=0, column=2)
+
+        self.color_button = tk.Button(self.root, text='color', command=self.choose_color)
+        self.color_button.grid(row=0, column=3)
+
+        self.eraser_button = tk.Button(self.root, text='eraser', command=self.use_eraser)
+        self.eraser_button.grid(row=0, column=4)
+
+        self.choose_size_button = tk.Scale(self.root, from_=1, to=10, orient=tk.HORIZONTAL, command=self.update_width)
+        self.choose_size_button.set(self.DEFAULT_PEN_SIZE)
+        self.choose_size_button.grid(row=0, column=5)
+
+        self.text_entry = tk.Entry(self.root, width=50, textvariable=self.text_input)
+        self.text_entry.grid(row=1, columnspan=5)
+        self.text_button = tk.Button(self.root, text='text', command=self.use_text)
+        self.text_button.grid(row=1, column=5)
+
+        # Initialize some stuff
+        self.setup()
+
+        # # create a toplevel menu
+        # menubar = tk.Menu(self.root)
+
+        # # create a pulldown menu, and add it to the menu bar
+        # filemenu = tk.Menu(menubar, tearoff=0)
+        # filemenu.add_command(label="Open", command=self.hello)
+        # filemenu.add_command(label="Save", command=self.hello)
+        # filemenu.add_separator()
+        # filemenu.add_command(label="Exit", command=self.root.quit)
+        # menubar.add_cascade(label="File", menu=filemenu)
+
+        # # create more pulldown menus
+        # editmenu = tk.Menu(menubar, tearoff=0)
+        # editmenu.add_command(label="Cut", command=self.hello)
+        # editmenu.add_command(label="Copy", command=self.hello)
+        # editmenu.add_command(label="Paste", command=self.hello)
+        # menubar.add_cascade(label="Edit", menu=editmenu)
+
+        # helpmenu = tk.Menu(menubar, tearoff=0)
+        # helpmenu.add_command(label="About", command=self.hello)
+        # menubar.add_cascade(label="Help", menu=helpmenu)
+
+        # # display the menu
+        # self.root.config(menu=menubar)
+
+
+        self.root.mainloop()
+
+    def setup(self):
+        self.start_x = None
+        self.start_y = None
+        self.ghost = None
+        self.color = self.DEFAULT_COLOR
+        self.active_button = self.pen_button
+        self.use_pen()
+        # self.c.bind('<Button-1>', self.draw_start)
+        # self.c.bind('<B1-Motion>', self.draw_motion)
+        # self.c.bind('<ButtonRelease-1>', self.draw_release)
+        # self.c.bind('<Motion>', self.motion)
+        # self.c.bind('<Leave>', self.reset)
+        # self.root.bind('<Key>', self.key_up)
+
+    def hello(self):
+        print("hello!")
+
+    def use_pen(self):
+        self.mode = 'pen'
+        self.activate_button(self.pen_button)
+        the_queue.put('mode {}'.format(self.mode).encode())
+
+    def use_rect(self):
+        self.mode = 'rectangle'
+        self.activate_button(self.rectangle_button)
+        the_queue.put('mode {}'.format(self.mode).encode())
+
+    def use_ellipse(self):
+        self.mode = 'ellipse'
+        self.activate_button(self.ellipse_button)
+        the_queue.put('mode {}'.format(self.mode).encode())
+
+    def choose_color(self):
+        color = askcolor(color=self.color)[1]
+        if not color:
+            return
+        self.color = color
+        the_queue.put('color {}'.format(self.color).encode())
+
+    def use_eraser(self):
+        self.mode = 'eraser'
+        self.activate_button(self.eraser_button)
+        the_queue.put('mode {}'.format(self.mode).encode())
+
+    def use_text(self):
+        self.mode = 'text'
+        self.activate_button(self.text_button)
+        the_queue.put('text {}'.format(self.text_input.get()).encode())
+        the_queue.put('mode {}'.format(self.mode).encode())
+
+    def activate_button(self, some_button):
+        self.active_button.config(relief=tk.RAISED)
+        some_button.config(relief=tk.SUNKEN)
+        self.active_button = some_button
+
+    def update_width(self, value):
+        value = int(value)
+        self.line_width = value
+        self.font.configure(size=(value * 5))
+        the_queue.put('width {}'.format(value).encode())
+
+
+class SocketThread(threading.Thread):
+
+    def __init__(self):
+        super().__init__()
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        while True:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                connected = False
+                i = 0
+                while not connected:
+                    i += 1
+                    try:
+                        s.connect((HOST, PORT))
+                        connected = True
+                        print('Commander socket online')
+                    except socket.error as e:
+                        print("Commander socket failed to connect (trial {})".format(i))
+                        print(e)
+                        time.sleep(2)
+
+                while True:
+                    while not the_queue.empty():
+                        message = the_queue.get(block=False)
+                        print('queue got:', message)
+                        s.sendall(message)
+            print('socket disconnected')
+
+
+if __name__ == '__main__':
+    SocketThread()
+    Commander()
--- painter.py
+++ painter.py
@@ -1,132 +1,83 @@
+import socket
+import queue
+import threading
 import tkinter as tk
-from tkinter.colorchooser import askcolor
 import tkinter.font as tkFont
 
 
-class Paint():
+the_queue = queue.Queue()
 
-    DEFAULT_PEN_SIZE = 5.0
-    DEFAULT_COLOR = 'black'
-    BG_COLOR = 'white'
+HOST = 'localhost'
+PORT = 4816
 
-    mode = 'pen'
+
+class Painter():
+
+    DEFAULT = {
+        'line': 5.0,
+        'color': '#000000',
+        'background': '#ffffff',
+        'mode': 'pen',
+    }
 
     def __init__(self):
         self.root = tk.Tk()
 
+        # Load config.json
+        # Place the window
+        # self.root.geometry("1000x900+10+10")
+
+        # Some variables
         self.items = []
-
         self.font = tkFont.Font(family='Helvetica', size=20)
-
         self.text_input = tk.StringVar(self.root)
 
-        self.pen_button = tk.Button(self.root, text='pen', command=self.use_pen)
-        self.pen_button.grid(row=0, column=0)
+        # The canvas
+        self.c = tk.Canvas(self.root, bg=self.DEFAULT['background'])
+        # self.c.grid(row=2, columnspan=6, sticky='nesw')
+        self.c.pack(expand=True, fill=tk.BOTH)
 
-        self.rectangle_button = tk.Button(self.root, text='rect', command=self.use_rect)
-        self.rectangle_button.grid(row=0, column=1)
-
-        self.ellipse_button = tk.Button(self.root, text='ellipse', command=self.use_ellipse)
-        self.ellipse_button.grid(row=0, column=2)
-
-        self.color_button = tk.Button(self.root, text='color', command=self.choose_color)
-        self.color_button.grid(row=0, column=3)
-
-        self.eraser_button = tk.Button(self.root, text='eraser', command=self.use_eraser)
-        self.eraser_button.grid(row=0, column=4)
-
-        self.choose_size_button = tk.Scale(self.root, from_=1, to=10, orient=tk.HORIZONTAL, command=self.update_width)
-        self.choose_size_button.set(self.DEFAULT_PEN_SIZE)
-        self.choose_size_button.grid(row=0, column=5)
-
-        self.text_entry = tk.Entry(self.root, width=50, textvariable=self.text_input)
-        self.text_entry.grid(row=1, columnspan=5)
-        self.text_button = tk.Button(self.root, text='text', command=self.use_text)
-        self.text_button.grid(row=1, column=5)
-
-        self.c = tk.Canvas(self.root, bg=self.BG_COLOR, width=600, height=600)
-        self.c.grid(row=2, columnspan=6)
-
+        # Initialize some stuff
         self.setup()
-
-        # # create a toplevel menu
-        # menubar = tk.Menu(self.root)
-
-        # # create a pulldown menu, and add it to the menu bar
-        # filemenu = tk.Menu(menubar, tearoff=0)
-        # filemenu.add_command(label="Open", command=self.hello)
-        # filemenu.add_command(label="Save", command=self.hello)
-        # filemenu.add_separator()
-        # filemenu.add_command(label="Exit", command=self.root.quit)
-        # menubar.add_cascade(label="File", menu=filemenu)
-
-        # # create more pulldown menus
-        # editmenu = tk.Menu(menubar, tearoff=0)
-        # editmenu.add_command(label="Cut", command=self.hello)
-        # editmenu.add_command(label="Copy", command=self.hello)
-        # editmenu.add_command(label="Paste", command=self.hello)
-        # menubar.add_cascade(label="Edit", menu=editmenu)
-
-        # helpmenu = tk.Menu(menubar, tearoff=0)
-        # helpmenu.add_command(label="About", command=self.hello)
-        # menubar.add_cascade(label="Help", menu=helpmenu)
-
-        # # display the menu
-        # self.root.config(menu=menubar)
 
         self.root.mainloop()
 
     def setup(self):
+        self.line_width = self.DEFAULT['line']
         self.start_x = None
         self.start_y = None
         self.ghost = None
-        self.line_width = self.choose_size_button.get()
-        self.color = self.DEFAULT_COLOR
-        self.eraser_on = False
-        self.active_button = self.pen_button
-        self.use_pen()
+        self.color = self.DEFAULT['color']
+        self.text_input.set('')
+        self.mode = self.DEFAULT['mode']
         self.c.bind('<Button-1>', self.draw_start)
         self.c.bind('<B1-Motion>', self.draw_motion)
         self.c.bind('<ButtonRelease-1>', self.draw_release)
         self.c.bind('<Motion>', self.motion)
         self.c.bind('<Leave>', self.reset)
         self.root.bind('<Key>', self.key_up)
+        self.root.after(100, self.check_queue)
 
-    def hello(self):
-        print("hello!")
-
-    def use_pen(self):
-        self.mode = 'pen'
-        self.activate_button(self.pen_button)
-
-    def use_rect(self):
-        self.mode = 'rectangle'
-        self.activate_button(self.rectangle_button)
-
-    def use_ellipse(self):
-        self.mode = 'ellipse'
-        self.activate_button(self.ellipse_button)
-
-    def choose_color(self):
-        self.color = askcolor(color=self.color)[1]
-
-    def use_eraser(self):
-        self.mode = 'eraser'
-        self.activate_button(self.eraser_button)
-
-    def use_text(self):
-        self.mode = 'text'
-        self.activate_button(self.text_button)
-
-    def activate_button(self, some_button):
-        self.active_button.config(relief=tk.RAISED)
-        some_button.config(relief=tk.SUNKEN)
-        self.active_button = some_button
-
-    def update_width(self, value):
-        value = int(value)
-        self.line_width = value
-        self.font.configure(size=(value * 5))
+    def check_queue(self):
+        while not the_queue.empty():
+            message = the_queue.get(block=False).split(' ', 1)
+            print('queue got', message)
+            # process message
+            if message[0] == 'color':
+                self.color = message[1]
+            elif message[0] == 'clean':
+                self.clean_canvas()
+            elif message[0] == 'mode':
+                self.mode = message[1]
+                self.reset(None)
+            elif message[0] == 'width':
+                width = int(message[1])
+                self.line_width = width
+                self.font.configure(size=(width * 5))
+            elif message[0] == 'text':
+                self.text_input.set(message[1])
+        # check again later
+        self.root.after(100, self.check_queue)
 
     def key_up(self, event):
         ctrl = (event.state & 0x4) != 0
@@ -136,8 +87,25 @@ class Paint():
                 self.c.delete(item)
                 self.items.pop()
         if event.keysym == 'w' and ctrl:
-            self.items.clear()
-            self.c.delete("all")
+            the_queue.put('clean')
+        if event.char == 'r':
+            the_queue.put('mode rectangle')
+        if event.char == 'e':
+            the_queue.put('mode ellipse')
+        if event.char == 'p':
+            the_queue.put('mode pen')
+        if event.char == 'l':
+            the_queue.put('mode line')
+        if event.char == '+':
+            value = int(min(10, self.line_width + 1))
+            the_queue.put('width {}'.format(value))
+        if event.char == '-':
+            value = int(max(1, self.line_width - 1))
+            the_queue.put('width {}'.format(value))
+
+    def clean_canvas(self):
+        self.items.clear()
+        self.c.delete("all")
 
     def reset(self, event):
         if self.ghost:
@@ -150,7 +118,7 @@ class Paint():
         if self.mode in ['pen', 'eraser']:
             if self.ghost:
                 self.c.delete(self.ghost)
-            width = self.choose_size_button.get() / 2
+            width = self.line_width / 2
             self.ghost = self.c.create_oval(event.x - width, event.y - width, event.x + width, event.y + width,
                                             outline='black', width=1)
         if self.mode == 'text':
@@ -201,9 +169,36 @@ class Paint():
             self.items.append(self.c.create_oval(self.start_x, self.start_y, event.x, event.y,
                                                  outline=self.color, width=self.line_width))
         if self.mode == 'text':
-            self.use_pen()
+            self.mode = 'pen'
         self.reset(None)
 
 
+class SocketThread(threading.Thread):
+
+    def __init__(self):
+        super().__init__()
+        self.daemon = True
+        self.start()
+
+    def run(self):
+
+        while True:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.bind((HOST, PORT))
+                s.listen()
+                conn, addr = s.accept()
+                with conn:
+                    print('Connected by', addr)
+                    while True:
+                        data = conn.recv(1024)
+                        if not data:
+                            break
+                        data = data.decode()
+                        print('received:', data)
+                        the_queue.put(data)
+            print('socket disconnected')
+
+
 if __name__ == '__main__':
-    Paint()
+    SocketThread()
+    Painter()
